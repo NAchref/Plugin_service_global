90e5629dffc3b4c372cac26e05c1e30c53f0545b
NAchref <achref.nefzaoui@esprit.tn>
2022-05-15 23:13:30 +0100

    commit work

--- template/services_collection.php
+++ template/services_collection.php
@@ -1,9 +1,4 @@
-<?php
-/**
- * @package service-global
- */
-?>
-
+<html>
 <?php
 function service_collection_table_html(){
   ob_start();
@@ -33,7 +28,10 @@ if(!empty($titre)){
 $rows = $wpdb->get_results("SELECT ID, TITRE, DESCRIPTION, ICONE, GALLERIE from $table_name");
 ?>
 <link type="text/css" href="<?php echo WP_PLUGIN_URL; ?>./service-global/assets/css/custom.css" rel="stylesheet" />
-<div class="section"> 
+<link type="text/css" href="<?php echo WP_PLUGIN_URL; ?>/service-global/assets/css/custom_singel.css" rel="stylesheet" />
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+
+<div class="section" id="section"> 
   <div>
     <p>filter par categorie: 
       <button class="btn">Immobilier</button>
@@ -50,7 +48,7 @@ $rows = $wpdb->get_results("SELECT ID, TITRE, DESCRIPTION, ICONE, GALLERIE from 
           <div class="grid">
             <span><input type="checkbox" name="titre" class="checkbox" value="<?php echo $row->titre; ?>"/><i class="fas fa-unlink"></i>&nbsp;&nbsp;<?php echo $row->TITRE; ?></span>
             <small><?php echo $row->DESCRIPTION; ?></small>
-            <a href="<?php echo ''; ?>">En savoir plus</a>
+            <a href="javascript:popUp();">En savoir plus</a>
           </div>
       <?php
       }?>
@@ -58,6 +56,47 @@ $rows = $wpdb->get_results("SELECT ID, TITRE, DESCRIPTION, ICONE, GALLERIE from 
     <input type='submit' name="insert" value='Valider la sélection' class='btn-selection'>
   </form>
 </div>
+
+
+<div class="float-container" style="display: none;" id="container"> 
+<div class="float-child grid-gallerie">
+    <div class="">
+    <img class="img-icon" src="<?php echo WP_PLUGIN_URL; ?>/service-global/media/city-solid.svg" alt="">
+    <h2>Location de bureau</h2>
+    <div class="bg-image-wrapper">
+    <div class="table">
+        <div class="row">
+            <div class="cell"><img src="<?php echo WP_PLUGIN_URL; ?>/service-global/media/service-media/col1.jpg" alt="" /></div>
+            <div class="cell"><img src="<?php echo WP_PLUGIN_URL; ?>/service-global/media/service-media/col2.jpg" alt="" /></div>
+        </div>
+        <div class="row">
+            <div class="cell"><img src="<?php echo WP_PLUGIN_URL; ?>/service-global/media/service-media/col3.jpg" alt="" /></div>
+            <div class="cell"><img src="<?php echo WP_PLUGIN_URL; ?>/service-global/media/service-media/col4.jpg" alt="" /></div>
+        </div>
+    </div>
+</div>
+    </div>
+  </div>
+  <div class="float-child grid-details">
+    <div class="">
+    <span>Détails</span>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quasi nulla eum delectus adipisci. Totam molestias saepe, quidem dolorum accusantium optio, sequi explicabo possimus odio nulla odit sed vero ipsumectus adipisci saepe, quidem dolorum accusantium optio, sequi explicabo possimus odio nulla odit sed vero ipsum.</p>
+    <label>Options</label><br>
+    <select name="option" id="option-select">
+        <option value="">100m2 - 2000£/mois </option>
+        <option value="">150m2 - 2500£/mois</option>
+        <option value="">200m2 - 3000£/mois</option>
+    </select><br>
+    <label for="story">Besoins supplémentaire</label><br>
+    <textarea id="" name=""
+          rows="5" cols="33">
+    </textarea>
+    <button class="btn-ss">Ajouter à la sélection</button>
+    </div>
+  </div>
+</div>
+
+
 <?php
 return ob_get_clean();
 }
@@ -67,3 +106,14 @@ define('ROOTDIR', plugin_dir_path(__FILE__));
 require_once(ROOTDIR . 'template/singel_page_service.php');
 
 ?>
+<script>
+
+function popUp(){
+  var container = document.getElementById('container');
+  var section = document.getElementById('section');
+  container.setAttribute('style', 'display: block');
+  section.setAttribute('style','opacity: 1;');
+}
+
+</script>
+
